bba3e24071b8216eac274664128dc5fd9a2b8977
Rakesh-ghb <rakeshrakeshgorre@gmail.com>
2025-05-14 21:04:27 +0530

    Add files via upload

---
+++ Example4.java
@@ -0,0 +1,10 @@
+package com.loop;
+
+public class Example4 {
+	public static void main(String [] args) {
+		for (int i = 1 ; i <= 15 ; i = i + 2) {
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ Example5.java
@@ -0,0 +1,10 @@
+package com.loop;
+
+public class Example5 {
+	public static void main(String [] args) {
+		for (int i = 5 ; i <= 50 ; i = i + 5) {
+			System.out.println(i);
+		}
+	}
+
+}
